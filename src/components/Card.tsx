e8f1f04381e3a2fd81592c1eb6c3c22bf35ce643
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2020-02-25 20:38:12 +0700

    Add card

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { default as MaterialCard } from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+
+const Card = () => {
+  return (
+    <MaterialCard>
+      <CardContent></CardContent>
+    </MaterialCard>
+  );
+};
+
+export default Card;
